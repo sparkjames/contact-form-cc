62297d2f916aa7e87f676b3fb1c96457527ea1f9
James Pfleiderer <jpfleiderer@gmail.com>
2020-12-13 11:29:59 -0500

    clean up comments

---
+++ tests/public/inc/formHandlerTest.php
@@ -0,0 +1,29 @@
+<?php
+// class FormHandlerTest extends PHPUnit_Framework_TestCase {
+
+// }
+
+
+declare(strict_types=1);
+
+use PHPUnit\Framework\TestCase;
+
+final class FormHandlerTest extends TestCase {
+
+    public function testProcessFormReturnsJSON(){
+
+		require_once '../../../public/inc/formHandler.class.php';
+		// require_once '../public/inc/formHandler.class.php';
+
+		$sampleResponse = [
+			'successful_submit' => false,
+			'errors' => [],
+		];
+		$sampleResponseJSON = json_encode($sampleResponse);
+
+		$formHandler = new FormHandler;
+
+		$this->assertEquals($sampleResponseJSON, $formHandler->processForm());
+	}
+
+}
